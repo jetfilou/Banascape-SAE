9069cedd63d1bb6a85808078a832098be74f6829
theoD <theodouchet7@gmail.com>
2024-06-19 21:54:11 +0200

    ajout comm menu principal et nouvelle partie

--- Banascape/FormMenuPrincipal.cs
+++ Banascape/FormMenuPrincipal.cs
@@ -2,11 +2,17 @@ namespace Banascape
 {
     public partial class frmMenuPrincipal : Form
     {
+        // Constructeur du formulaire frmMenuPrincipal
         public frmMenuPrincipal()
         {
             InitializeComponent();
         }
 
+        // Gestionnaire d'événements Click pour le bouton Quitter
+        // Affiche une boîte de dialogue de confirmation et quitte l'application si l'utilisateur confirme
+        // paramètre :
+        //    sender : objet source de l'événement
+        //    e : arguments de l'événement
         private void btnQuitter_Click(object sender, EventArgs e)
         {
             DialogResult resultat = MessageBox.Show("Voulez-vous vraiment quitter le jeu ?",
@@ -19,6 +25,11 @@ namespace Banascape
             }
         }
 
+        // Gestionnaire d'événements Click pour le bouton Nouvelle Partie
+        // Instancie et affiche le formulaire frmNouvellePartie, puis cache le formulaire actuel
+        // paramètre :
+        //    sender : objet source de l'événement
+        //    e : arguments de l'événement
         private void btnNouvellePartie_Click(object sender, EventArgs e)
         {
             frmNouvellePartie frmNewPartie;
@@ -31,6 +42,11 @@ namespace Banascape
 
         }
 
+        // Gestionnaire d'événements Click pour le bouton Classement
+        // Affiche le formulaire frmClassement, ou le met au premier plan s'il est déjà ouvert
+        // paramètre :
+        //    sender : objet source de l'événement
+        //    e : arguments de l'événement
         private void btnClassement_Click(object sender, EventArgs e)
         {
             Form frmClassement = Application.OpenForms["frmClassement"];
@@ -45,6 +61,11 @@ namespace Banascape
             }
         }
 
+        // Gestionnaire d'événements Click pour le bouton Reprendre
+        // Instancie et affiche le formulaire frmInterfaceJeu, puis cache le formulaire actuel
+        // paramètre :
+        //    sender : objet source de l'événement
+        //    e : arguments de l'événement
         private void btnReprendre_Click(object sender, EventArgs e)
         {
             frmInterfaceJeu frmjeu;
--- Banascape/FormNouvellePartie.cs
+++ Banascape/FormNouvellePartie.cs
@@ -2,7 +2,11 @@
 {
     public partial class frmNouvellePartie : Form
     {
+        //Déclaration de la variable Difficulte
         bool difficulte;
+
+        // Constructeur du formulaire frmNouvellePartie
+        // Cache les labels d'alerte
         public frmNouvellePartie()
         {
             InitializeComponent();
@@ -10,6 +14,11 @@
             lblAlertDifficulter.Hide();
         }
 
+        // Gestionnaire d'événements Click pour le bouton Retour
+        // Instancie et affiche le formulaire frmMenuPrincipal, puis ferme le formulaire actuel
+        // paramètre :
+        //    sender : objet source de l'événement
+        //    e : arguments de l'événement
         private void btnRetour_Click(object sender, EventArgs e)
         {
             frmMenuPrincipal frmMenuPrincipal;
@@ -17,6 +26,12 @@
             frmMenuPrincipal.Show();
             this.Close();
         }
+
+        // Gestionnaire d'événements Click pour le bouton Lancer
+        // Vérifie si un pseudo et une difficulté sont sélectionnés avant de lancer le jeu
+        // paramètre :
+        //    sender : objet source de l'événement
+        //    e : arguments de l'événement
         private void btnLancer_Click(object sender, EventArgs e)
         {
             if (txtPseudo.Text != "")
