7e74f4e27c14eaeb91da87e849df2167ffd65870
theoD <theodouchet7@gmail.com>
2024-06-19 21:42:23 +0200

    ajout comm menu echap

--- Banascape/FormMenuEchap.cs
+++ Banascape/FormMenuEchap.cs
@@ -2,6 +2,8 @@
 {
     public partial class FormMenuEchap : Form
     {
+        // Constructeur du formulaire FormMenuEchap
+        // Initialise le composant et configure les gestionnaires d'événements pour les touches
         public FormMenuEchap()
         {
             InitializeComponent();
@@ -10,6 +12,11 @@
             this.KeyPreview = true;
         }
 
+        // Gestionnaire d'événements touche presser pour la touche Echap
+        // Cache le formulaire si la touche Échap est pressée
+        // paramètre :
+        //    sender : objet source de l'événement
+        //    e : arguments de l'événement
         private void FormMenuEchap_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Escape)
@@ -18,11 +25,21 @@
             }
         }
 
+        // Gestionnaire d'événements Click pour le bouton Play
+        // Cache le formulaire quand le bouton Play est cliqué
+        // paramètre :
+        //    sender : objet source de l'événement
+        //    e : arguments de l'événement
         private void btnPlay_Click(object sender, EventArgs e)
         {
             this.Hide();
         }
 
+        // Gestionnaire d'événements Click pour le bouton Quitter
+        // Affiche une boîte de dialogue de confirmation et quitte l'application si l'utilisateur confirme
+        // paramètre :
+        //    sender : objet source de l'événement
+        //    e : arguments de l'événement
         private void btnQuitter_Click(object sender, EventArgs e)
         {
             DialogResult resultat = MessageBox.Show("Voulez-vous vraiment quitter le jeu ?", "Confirmation",
@@ -34,6 +51,11 @@
             }
         }
 
+        // Gestionnaire d'événements Click pour le bouton Retour au Menu Principal
+        // Affiche une boîte de dialogue de confirmation et retourne au menu principal si l'utilisateur confirme
+        // paramètre :
+        //    sender : objet source de l'événement
+        //    e : arguments de l'événement
         private void btnRetourMenuPrincipal_Click(object sender, EventArgs e)
         {
             DialogResult resultat = MessageBox.Show("Voulez-vous vraiment retourner au menu Principal ?", "Confirmation",
